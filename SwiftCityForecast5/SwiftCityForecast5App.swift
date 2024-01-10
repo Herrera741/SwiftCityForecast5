58e2688a1d241c8dbc3ebca4d5990fafe772d910
Sergio <sherrera725@gmail.com>
2024-01-10 15:32:02 -0800

    Initial Commit

---
+++ SwiftCityForecast5/SwiftCityForecast5App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCityForecast5App.swift
+//  SwiftCityForecast5
+//
+//  Created by Sergio Herrera on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCityForecast5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
