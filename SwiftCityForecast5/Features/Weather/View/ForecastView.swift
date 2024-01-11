12214f62158ac35ffa76dbbe2c9db07f42b079ec
Sergio <sherrera725@gmail.com>
2024-01-10 16:04:49 -0800

    add initial project files and setup app file structure

--- SwiftCityForecast5/Features/Weather/View/ForecastView.swift
+++ SwiftCityForecast5/Features/Weather/View/ForecastView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  WeatherView.swift
 //  SwiftCityForecast5
 //
 //  Created by Sergio Herrera on 1/10/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ForecastView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ForecastView()
 }
