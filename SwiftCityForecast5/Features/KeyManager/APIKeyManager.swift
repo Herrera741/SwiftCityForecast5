c3c9990c558397afb8e646cdfe89936386445168
Sergio <sherrera725@gmail.com>
2024-01-24 07:48:47 -0800

    add APIKeyManager code block to file for fetching api key from Firestore

--- SwiftCityForecast5/Features/KeyManager/APIKeyManager.swift
+++ SwiftCityForecast5/Features/KeyManager/APIKeyManager.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class APIKeyManager {
+    private let apiCollection = Firestore.firestore().collection("api_keys")
+    
+    func fetchKey(completion: @escaping (String) -> Void) {
+        let weatherDocRef = apiCollection.document("weather")
+        
+        weatherDocRef.getDocument { document, error in
+            if let document = document, document.exists {
+                let data = document.data()
+
+                // Use optional binding to safely unwrap and cast to String
+                if let apiKey = data?["api_key"] as? String {
+                    completion(apiKey)
+                } else {
+                    print("api_key is not a String or is nil")
+                }
+            } else {
+                print("Document doesn't exist")
+            }
+        }
+    }
+}
